f841d3d6fe1a45bc4eb5b4d9a377125a6e13fb22
techlover9922 <techlover9922@gmail.com>
2024-05-09 18:54:46 -0700

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,20 @@
-// Step 1: Import React
 import { Link } from 'gatsby'
 import * as React from 'react'
 import Layout from '../components/layout'
-// Step 2: Define your component
+import { StaticImage } from 'gatsby-plugin-image'
 const IndexPage = () => {
   return (
     <Layout>
       <h1>Home Page</h1>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
     </Layout>
   )
 }
 
-// You'll learn about this in the next task, just copy it for now
 export const Head = () => <title>Home</title>
-
-// Step 3: Export your component
 export default IndexPage
