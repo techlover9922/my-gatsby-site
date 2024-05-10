3bbb91230cd7eec182262e88a68d998b6d086e9f
techlover9922 <techlover9922@gmail.com>
2024-05-09 18:25:54 -0700

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about/about.js
@@ -0,0 +1,13 @@
+import { Link } from "gatsby";
+import * as React from "react";
+
+export default function Component () {
+    return <main>
+        <h1>"Hello world"</h1>
+        </main>
+  }
+
+// export const Head = () => <>
+//     <title>About Me</title>
+//     <meta name = "description" content = "your description" />
+// </>
---
+++ src/pages/about/index.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import About from './about'
+import Layout from '../../components/layout';
+export default () => {
+    return (<Layout>
+        <About/>
+    </Layout>)
+}
